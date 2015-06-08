0d0f25da35950f21a4c43e0d044698742cfdecb9
Rob Howard <rob@robhoward.id.au>
2015-06-09 01:50:13 +1000

    Adds a default set of ships to muck around with.

--- src/Data/Battleship.hs
+++ src/Data/Battleship.hs
@@ -8,6 +8,14 @@ data Ship     = Ship String (Int,Int)          deriving(Show)
 data Player   = Player1 | Player2              deriving(Show)
 data Board    = Board [(Player,Ship,Position)] deriving(Show)
 
+defaultShips = map (\(n,p) -> Ship n p) [
+                 ("Carrier",    (1,5)),
+                 ("Battleship", (1,4)),
+                 ("Submarine",  (1,3)),
+                 ("Cruiser",    (1,2)),
+                 ("Patrol",     (1,1)),
+               ]
+
 -- First stab: branching, rather than encoding board states in types.
 
 finished :: Board -> Bool
