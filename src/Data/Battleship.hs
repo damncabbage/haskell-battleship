b842e5a5ea9fbf0d0eebdc2b9843e484d60d051b
Rob Howard <rob@robhoward.id.au>
2015-06-16 00:11:48 +1000

    [ noodling intensifies ] (Railscamp)

--- src/Data/Battleship.hs
+++ src/Data/Battleship.hs
@@ -1,26 +1,29 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Data.Battleship (
-  Orientation, Result, Ship, Player, Coordinates, ShipPlacement, Shot, Board, Game,
-  mkEmptyBoard, mkRandomBoard, placeShip, attack, finished, winner
+  Orientation, Result, Ship, Player, Coords, ShipPlacement, Shot, Board, Game, BoardDimensions,
+  defaultShips, shipsFromList,
+  mkEmptyBoard, mkRandomBoard, mkCoords,
+  placeShip, attack, finished, winner
 ) where
 
 import Data.Maybe
+import Data.Monoid
 import System.Random
 
 -- TODO: Custom Shows
-data Orientation = Down | Right                      deriving(Show)
-data Result      = Hit Ship | Miss                   deriving(Show)
-data Ship        = Ship String (Int,Int)             deriving(Show)
-data Player      = Player1 | Player2                 deriving(Show)
+data Orientation = Down | Right          deriving(Show)
+data Result      = Hit Ship | Miss       deriving(Show)
+data Ship        = Ship String (Int,Int) deriving(Show)
+data Player      = Player1 | Player2     deriving(Show)
+data Coords      = Coords Int Int        deriving(Show)
 
--- TODO: Coords can't be a type alias; we need to have a restricted constructor, otherwise
---       we'll allow (-1,900000) or something.
-type Coordinates   = (Int,Int)
-type BoardSize     = (Int,Int)
-type ShipPlacement = (Ship,Coordinates,Orientation)
-type Shot          = (Player,Coordinates)
+type BoardDimensions = (Int,Int)
+type ShipPlacement   = (Ship,Coords,Orientation)
+type Shot            = (Player,Coords)
 
 data Board = Board {
-               size       :: BoardSize,
+               size       :: BoardDimensions,
                placements :: [ShipPlacement],
                shots      :: [(Shot,Result)]
              } deriving(Show)
@@ -31,7 +34,7 @@ data Game  = Game {
              } deriving(Show)
 
 defaultShips :: [Ship]
-defaultShips = map (\(n,p) -> Ship n p) [
+defaultShips = shipsFromList [
                  ("Carrier",    (1,5)),
                  ("Battleship", (1,4)),
                  ("Submarine",  (1,3)),
@@ -39,23 +42,40 @@ defaultShips = map (\(n,p) -> Ship n p) [
                  ("Patrol",     (1,1))
                ]
 
+-- TODO: Another Maybe? Not constraining creation of ships.
+shipsFromList :: [(String,(Int,Int))] -> [Ship]
+shipsFromList = map (\(n,p) -> Ship n p)
+
 -- Headlining operations
 -- First stab: branching, rather than encoding board states in types.
 -- TODO: This is all assuming a single board; need to encode the board pair with ownerships.
-mkEmptyBoard :: BoardSize -> Maybe Board
-mkEmptyBoard size =
-  Just Board { size = size, placements = [], shots = [] }
+mkEmptyBoard :: BoardDimensions -> Maybe Board
+mkEmptyBoard size@(x,y)
+  | x > 0 && y > 0 = Just Board { size = size, placements = [], shots = [] }
+  | otherwise      = Nothing
 
-mkRandomBoard :: BoardSize -> StdGen -> Maybe Board
-mkRandomBoard size gen = do
+mkRandomBoard :: BoardDimensions -> [Ship] -> (IO StdGen) -> Maybe Board
+mkRandomBoard size ships gen = do
   board <- mkEmptyBoard size
   error "TODO"
 
-mkCoordinates :: (Int,Int) -> BoardSize -> Maybe Coordinates
-mkCoordinates (cx,cy) (bw,bh) = error "TODO"
+mkCoords :: (Int,Int) -> Board -> Maybe Coords
+mkCoords coords@(cx,cy) (size -> (bx,by))
+    | valid cx bx && valid cy by = Just (Coords cx cy)
+    | otherwise                  = Nothing
+  where
+    valid c b = c > 0 && c <= b
 
-placeShip :: Board -> ShipPlacement -> Board
-placeShip = error "TODO"
+-- (Ship,Coords,Orientation)
+placeShip :: Board -> ShipPlacement -> Maybe Board
+placeShip b p
+    | validPlacement b p = Just b { placements = placements b <> [p] } -- TODO: Replace with Lens in three years.
+    | otherwise          = Nothing
+  where
+    validPlacement b p = noOverlaps (placements b) p && inBounds (size b) p
+    noOverlaps ps pnew@(ship,coords,orient) = error "TODO"
+    inBounds b pnew@(ship,coords,orient)    = error "TODO"
+
 
 attack :: Game -> Player -> Shot -> Game
 attack = error "TODO"
--- test/tests.hs
+++ test/tests.hs
@@ -5,6 +5,27 @@ import           Data.Battleship
 import           Test.Hspec
 import           Test.Hspec.QuickCheck           (prop)
 import           Test.QuickCheck
+import           Data.Functor
+import           Data.Maybe
+import           Control.Applicative
+
+-- TODO Properties:
+-- - placeShip overlapping existing piece is nothing
+-- - placeShip not overlapping existing piece is a board with the placement
+
+-- TODO Arbitraries:
+instance Arbitrary Board where
+  arbitrary = (fromJust . mkEmptyBoard) <$> genDimensions
+  -- TODO: arbitrary = (\x -> Board { size = x, placements = [], shots = [] }) <$> genDimensions
+
+genDimensions :: Gen BoardDimensions
+genDimensions = elements [(10,10)]
+
+-- - blank board
+-- - board with pre-placed pieces that do not overlap
+
+showBoard :: Board -> String
+showBoard = show
 
 main :: IO ()
 main = hspec $ do
@@ -15,4 +36,7 @@ main = hspec $ do
   prop "quickcheck hello world" $
     \x -> (read . show) x === (x :: String)
 
+  describe "placeShip" $ do
+    prop "overlapping ships produces a failure result" $ do
+      \x -> showBoard x === show (fromJust (mkEmptyBoard (10,10)))
 
