59f042278390889eaa06582ef421d25460606692
Rob Howard <rob@robhoward.id.au>
2015-06-17 02:14:06 +1000

    Adds safe creation of ships, and a board-size validation helper.

--- src/Data/Battleship.hs
+++ src/Data/Battleship.hs
@@ -1,40 +1,46 @@
 {-# LANGUAGE ViewPatterns #-}
 
 module Data.Battleship (
-  Orientation, Result, Ship, Player, Coords, ShipPlacement, Shot, Board, Game, BoardDimensions,
+  Orientation, Result, Player, Coords, Ship, ShipPlacement, Shot, Board, Game, Dimensions,
   defaultShips, shipsFromList,
   mkEmptyBoard, mkRandomBoard, mkCoords,
-  placeShip, attack, finished, winner
+  placeShip, attack, finished, winner,
+  boardLargeEnoughForShips
 ) where
 
 import Data.Maybe
 import Data.Monoid
+import Data.List (maximum)
 import System.Random
 
 -- TODO: Custom Shows
 data Orientation = Down | Right          deriving(Show)
 data Result      = Hit Ship | Miss       deriving(Show)
-data Ship        = Ship String (Int,Int) deriving(Show)
 data Player      = Player1 | Player2     deriving(Show)
 data Coords      = Coords Int Int        deriving(Show)
+data Ship        = Ship {
+                     name           :: String,
+                     shipDimensions :: Dimensions
+                   } deriving(Show)
 
-type BoardDimensions = (Int,Int)
+type Dimensions      = (Int,Int)
 type ShipPlacement   = (Ship,Coords,Orientation)
 type Shot            = (Player,Coords)
 
 data Board = Board {
-               size       :: BoardDimensions,
-               placements :: [ShipPlacement],
-               shots      :: [(Shot,Result)]
+               boardDimensions :: Dimensions,
+               placements      :: [ShipPlacement],
+               shots           :: [(Shot,Result)],
+               validShips      :: [Ship]
              } deriving(Show)
 data Game  = Game {
                player1    :: (Player,Board),
-               player2    :: (Player,Board),
-               validShips :: [Ship]
+               player2    :: (Player,Board)
              } deriving(Show)
 
+-- TODO: One of the few cases I'd consider using a fromJust.
 defaultShips :: [Ship]
-defaultShips = shipsFromList [
+defaultShips = fromMaybe [] $ shipsFromList [
                  ("Carrier",    (1,5)),
                  ("Battleship", (1,4)),
                  ("Submarine",  (1,3)),
@@ -43,24 +49,40 @@ defaultShips = shipsFromList [
                ]
 
 -- TODO: Another Maybe? Not constraining creation of ships.
-shipsFromList :: [(String,(Int,Int))] -> [Ship]
-shipsFromList = map (\(n,p) -> Ship n p)
+shipsFromList :: [(String,(Int,Int))] -> Maybe [Ship]
+shipsFromList = sequence . map (\(n,p) -> mkShip n p)
 
--- Headlining operations
--- First stab: branching, rather than encoding board states in types.
--- TODO: This is all assuming a single board; need to encode the board pair with ownerships.
-mkEmptyBoard :: BoardDimensions -> Maybe Board
-mkEmptyBoard size@(x,y)
-  | x > 0 && y > 0 = Just Board { size = size, placements = [], shots = [] }
-  | otherwise      = Nothing
+mkShip :: String -> (Int,Int) -> Maybe Ship
+mkShip n d@(x,y)
+  | x > 0 && y > 0 && length n > 0 = Just Ship { name = n, shipDimensions = d }
+  | otherwise                      = Nothing
 
-mkRandomBoard :: BoardDimensions -> [Ship] -> (IO StdGen) -> Maybe Board
-mkRandomBoard size ships gen = do
-  board <- mkEmptyBoard size
+-- TODO: Appears to hold up for everything I've tossed at it. Needs a property test,
+--       but it's damn hard to come up with a generator producing non-overlapping ships.
+boardLargeEnoughForShips :: Dimensions -> [Ship] -> Bool
+boardLargeEnoughForShips (x,y) ships =
+    (largestDim >= longestShip) && (x * y >= shipsArea)
+  where
+    largestDim  = max x y
+    longestShip = (maximum . mconcat . map (pairToList . shipDimensions)) ships
+    shipsArea   = (sum . map (multList . pairToList . shipDimensions)) ships
+    multList    = foldr (*) 1
+    pairToList (a,b) = [a,b]
+
+mkEmptyBoard :: Dimensions -> [Ship] -> Maybe Board
+mkEmptyBoard d s =
+  if not (null s) && boardLargeEnoughForShips d s then
+    Just Board { boardDimensions = d, placements = [], shots = [], validShips = s }
+  else
+    Nothing
+
+mkRandomBoard :: Dimensions -> [Ship] -> (IO StdGen) -> Maybe Board
+mkRandomBoard dims ships gen = do
+  board <- mkEmptyBoard dims ships
   error "TODO"
 
 mkCoords :: (Int,Int) -> Board -> Maybe Coords
-mkCoords coords@(cx,cy) (size -> (bx,by))
+mkCoords coords@(cx,cy) (boardDimensions -> (bx,by))
     | valid cx bx && valid cy by = Just (Coords cx cy)
     | otherwise                  = Nothing
   where
@@ -69,10 +91,10 @@ mkCoords coords@(cx,cy) (size -> (bx,by))
 -- (Ship,Coords,Orientation)
 placeShip :: Board -> ShipPlacement -> Maybe Board
 placeShip b p
-    | validPlacement b p = Just b { placements = placements b <> [p] } -- TODO: Replace with Lens in three years.
+    | validPlacement b p = Just b { placements = placements b <> [p] } -- TODO: Replace with Lens in two years.
     | otherwise          = Nothing
   where
-    validPlacement b p = noOverlaps (placements b) p && inBounds (size b) p
+    validPlacement b p = noOverlaps (placements b) p && inBounds (boardDimensions b) p
     noOverlaps ps pnew@(ship,coords,orient) = error "TODO"
     inBounds b pnew@(ship,coords,orient)    = error "TODO"
 
--- test/tests.hs
+++ test/tests.hs
@@ -7,19 +7,21 @@ import           Test.Hspec.QuickCheck           (prop)
 import           Test.QuickCheck
 import           Data.Functor
 import           Data.Maybe
-import           Control.Applicative
 
 -- TODO Properties:
 -- - placeShip overlapping existing piece is nothing
 -- - placeShip not overlapping existing piece is a board with the placement
 
 -- TODO Arbitraries:
-instance Arbitrary Board where
-  arbitrary = (fromJust . mkEmptyBoard) <$> genDimensions
-  -- TODO: arbitrary = (\x -> Board { size = x, placements = [], shots = [] }) <$> genDimensions
-
-genDimensions :: Gen BoardDimensions
-genDimensions = elements [(10,10)]
+-- instance Arbitrary Board where
+--   arbitrary = (fromJust . mkEmptyBoard) <$> genDimensions
+--   -- TODO: arbitrary = (\x -> Board { size = x, placements = [], shots = [] }) <$> genDimensions
+--
+-- genDimensions :: Gen Dimensions
+-- genDimensions = elements [(10,10)]
+--
+-- genShips :: Gen [Ship]
+-- genShips = error "TODO"
 
 -- - blank board
 -- - board with pre-placed pieces that do not overlap
@@ -36,7 +38,20 @@ main = hspec $ do
   prop "quickcheck hello world" $
     \x -> (read . show) x === (x :: String)
 
-  describe "placeShip" $ do
-    prop "overlapping ships produces a failure result" $ do
-      \x -> showBoard x === show (fromJust (mkEmptyBoard (10,10)))
+--describe "placeShip" $ do
+--  prop "overlapping ships produces a failure result" $ do
+--    \x -> showBoard x === show (fromJust (mkEmptyBoard (10,10) []))
 
+  describe "boardLargeEnoughForShips" $ do
+    it "uses the longest ship and ship area to determine the minimum size" $ do
+      boardLargeEnoughForShips (5,3) ships `shouldBe` True
+      boardLargeEnoughForShips (4,3) ships `shouldBe` False
+      boardLargeEnoughForShips (5,2) ships `shouldBe` False
+      where
+        ships = fromJust $ shipsFromList [
+                             ("Carrier",    (1,5)),
+                             ("Battleship", (1,4)),
+                             ("Submarine",  (1,3)),
+                             ("Cruiser",    (1,2)),
+                             ("Patrol",     (1,1))
+                           ]
